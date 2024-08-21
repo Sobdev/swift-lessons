dbce7c5c6972afe68cecf90ff5aef44f824acf74
Sobdev <71671270+Sobdev@users.noreply.github.com>
2024-08-21 16:06:26 +0200

    first steps with variables

---
+++ first-steps/test.swift
@@ -0,0 +1,18 @@
+print("Hello World")
+
+var test: String = "Test with var"
+let test2: Int = 10
+
+var myVariable: Int = 42
+myVariable = 50
+let myConstant: Int = 42
+
+let implicitInteger: Int = 70
+let implicitDouble: Double = 70.0
+let explicitDouble: Double = 70
+
+let label: String = "The width is "
+let number: Int = 76
+let widthLabel: String = label + String(number)
+
+// You change the value of an existing variable to another value of a compatible type. You can't change the type of a variable after it's declared. You can't change the value of a constant after it's set.
